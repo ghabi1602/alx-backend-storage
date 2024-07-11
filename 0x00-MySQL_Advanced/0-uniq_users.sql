0f2c260eb4a45af9492f86c8eaf2e3bae00c5cb5
Abdessattar <ghabi.1602@gmail.com>
2024-07-11 23:52:26 +0000

    Update

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Script that creates the table users
+-- with certain requirements
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	)
