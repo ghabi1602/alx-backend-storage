ec38ab8f8a4d73938254c6000e4b7de4ea482374
Abdessattar <ghabi.1602@gmail.com>
2024-07-13 09:28:12 +0000

    UPdate

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 --Script that creates a table users
 --following certain criteria
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255),
