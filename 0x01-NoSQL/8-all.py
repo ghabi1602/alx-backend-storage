c9e3b1a13621a33114d8b26663a1962aba0b9cc8
Abdessattar <ghabi.1602@gmail.com>
2024-07-18 10:18:03 -0700

    Update

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""a pymongo function that lists all documents in a collection"""
+import pprint
+
+
+def list_all(mongo_collection):
+    """a function that lists all documents of a collection"""
+    docs = []
+    for doc in mongo_collection.find():
+            docs.append(doc)
+    return docs
+
