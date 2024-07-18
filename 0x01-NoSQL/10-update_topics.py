c09355f7716ce1afe15d55707bed6ab0d4ac42af
Abdessattar <ghabi.1602@gmail.com>
2024-07-18 15:19:49 -0700

    Update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""a python module that updates the topics of a school collection"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """a function that updates the topics of a school"""
+    mongo_collection.update_many(
+            {"name": name},
+            {"$push": {"topics": topics}}
+            )
