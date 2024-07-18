a319c5ea47ae2bb6e0bfa2b1d01a2838e517a96a
Abdessattar <ghabi.1602@gmail.com>
2024-07-18 15:41:20 -0700

    Update

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a module that returns the list of documents having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """a function that returns a list of schools having specific topic"""
+    schools = list(mongo_collection.find({"topic": topic}))
+    return schools
